a40611d2988ef5971d64418c8f30411426913fa1
Tailan Souza <tailansouza.dev@gmail.com>
2024-04-24 19:59:03 -0300

    .teste

---
+++ aula6-7-8/aula6.js
@@ -0,0 +1,9 @@
+const nome ='luiz';
+const sobreNome = 'otavio';
+let idade = 30;
+let peso = 84;
+let altura = 1.80; 
+let anoDeNacimento = (2019 - idade);
+let imc =peso/(altura*altura) ;
+
+console.log(nome,sobreNome,'tem',idade,'anos e pesa ',peso,'kg','tem',altura,'de altura e seu imc e de',imc,'\nele naceu em',anoDeNacimento);
