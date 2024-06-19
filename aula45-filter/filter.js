89b69cf8373eeaa4f66c2bd8eda0c4ab15d4a1ab
Tailan Souza <148594602+tailansouza@users.noreply.github.com>
2024-06-18 21:49:19 -0300

    fillter

---
+++ aula45-filter/filter.js
@@ -0,0 +1,23 @@
+//filter nao modifica valor so filtra
+const numeros = [5, 50, 80, 1, 2, 3, 4, 5, 10, 15, 30];
+
+const filtrados = numeros.filter(valor=> valor > 10);
+
+//console.log(filtrados)
+
+const pessoas = [
+    {nome:'tailan',  idade: 19},
+    {nome:'bbbb',  idade: 60},
+    {nome:'ccccc',  idade: 20},
+    {nome:'dddd',  idade: 85},
+    {nome:'eeeeee',  idade: 35},
+];
+
+const p2 = pessoas.filter(obj => obj.idade > 50 );
+
+
+const p3 = pessoas.filter(obj => obj.nome.toLowerCase().endsWith('n') );
+
+console.log(p3)
+
+
