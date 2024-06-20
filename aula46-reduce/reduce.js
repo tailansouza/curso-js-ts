064d6e8423c22c195c04c09a2cf48d2893844b56
Tailan Souza <148594602+tailansouza@users.noreply.github.com>
2024-06-19 21:20:37 -0300

    reduce

---
+++ aula46-reduce/reduce.js
@@ -0,0 +1,23 @@
+const numeros = [50, 80, 2, 56, 23, 45, 78, 25, 8];
+
+const total = numeros.reduce(function (acumulador,valor,indice,array){
+    acumulador.push(valor*2)
+return acumulador
+},[])
+//console.log(total)
+
+const pessoas = [
+    { nome: 'aaa', idade: 19 },
+    { nome: 'bbbb', idade: 60 },
+    { nome: 'ccccc', idade: 20 },
+    { nome: 'dddddd', idade: 85 },
+    { nome: 'eeeeeee', idade: 35 },
+];
+
+const maisVelha = pessoas.reduce(function(acumulador,valor){
+
+if (acumulador.idade > valor.idade)return acumulador ;
+return valor
+
+})
+console.log(maisVelha.nome +' '+ maisVelha.idade)
